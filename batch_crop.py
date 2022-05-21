6818dff69278276fc498dd732c053eb6e503f21d
Michael Otty <michael.otty@gmail.com>
2022-05-21 14:43:31 +0100

    Merge all pages into one output file

--- batch_crop.py
+++ batch_crop.py
@@ -14,18 +14,21 @@ def main():
         Path,
         filedialog.askopenfilenames(title='Choose files to be cropped...',
                                     filetypes=(('PDFs', '.pdf'), )))
+    
+    file_names = list(file_names)
+
+    writer = PdfFileWriter()
 
     for file_name in file_names:
         reader = PdfFileReader(str(file_name))
-        output_page = reader.getPage(0)
-        print(output_page.mediaBox)
-        output_page.mediaBox.lowerLeft = (
-            0, output_page.mediaBox.getUpperLeft_y() / 2)
-        writer = PdfFileWriter()
-        writer.addPage(output_page)
+        for page_number in range(reader.getNumPages()):
+            output_page = reader.getPage(page_number)
+            output_page.mediaBox.lowerLeft = (
+                0, output_page.mediaBox.getUpperLeft_y() / 2)
+            writer.addPage(output_page)
 
-        with open(file_name.with_stem('cropped_' + file_name.stem), 'wb') as f:
-            writer.write(f)
+    with open(file_names[0].with_stem('cropped_labels'), 'wb') as f:
+        writer.write(f)
 
 
 if __name__ == '__main__':
